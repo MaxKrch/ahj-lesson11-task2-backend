7fd38d91787387995bd3566389822a17ce1161a1
Max Kyrchanov <max.kyrchanov@gmail.com>
2024-07-12 01:20:07 +0500

    fix vercel.json

---
+++ index.js
@@ -0,0 +1,23 @@
+const Koa = require('koa');
+const cors = require('@koa/cors');
+const { koaBody } = require('koa-body');
+
+const router = require('./app/routes/index.js');
+
+const server = new Koa();
+
+server
+	.use(cors())
+	.use(koaBody({
+		text: true,
+		urlencoded: true,
+		multipart: true,
+		json: true,
+	}))
+
+server
+	.use(router())
+
+const port = process.env.PORT || 7070;
+server
+	.listen(port)
